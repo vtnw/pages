b4af28d05deb70c61ca38e15677a408bde519700
vtnw <vtnw@users.noreply.github.com>
2018-06-28 10:40:43 +0530

    Update link.js

--- scripts/link.js
+++ scripts/link.js
@@ -87,9 +87,9 @@ function loadLinks() {
             a.setAttribute("href", "#type");
             a.setAttribute("style", "line-height:30px;color:#aaaaaa;font-style:italic;text-decoration:none;");
             a.innerHTML = currType;
-            //d.appendChild(document.createElement('hr'));;
+            d.appendChild(document.createElement('hr'));
+            d.appendChild(a);
             d.appendChild(document.createElement('br'));
-            d.appendChild(a)
         }                
         var a = document.createElement('a');
         a.setAttribute("href", items[i].link);
