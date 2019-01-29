505231caf0be5feb9ceb8af7d623b37baee82940
vtnw <vtnw@users.noreply.github.com>
2019-01-29 11:19:58 +0530

    Update nsw.js

--- nsw.js
+++ nsw.js
@@ -1,15 +1,3 @@
 self.addEventListener('notificationclick', function(event) {
-  event.waitUntil({
-    const url = "https://vtnw.github.io/pages/note.html?n=" + event.notification.title;
-    const promiseChain = clients.matchAll({
-      type: 'window',
-      includeUncontrolled: true
-    })
-    .then((windowClients) => {
-      if(windowClients.length > 0){
-        windowClients[windowClients.length-1].focus();
-      } else {
-        return clients.openWindow(url);
-      }
-    });
+  event.waitUntil(clients.openWindow("https://vtnw.github.io/pages/note.html?m=view&n=" + event.notification.title));
 });
