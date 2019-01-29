ab174ed23afb0b80efaa6ada5198aad2508a48c3
vtnw <vtnw@users.noreply.github.com>
2019-01-29 09:35:16 +0530

    Update note.js

--- scripts/note.js
+++ scripts/note.js
@@ -1,14 +1,17 @@
 var u = decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent("u").replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
 var m = decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent("m").replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
+var n = decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent("n").replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
 var cacheName = "note" + u;
 var indexName = "index" + cacheName;
 var noteList = [];
 var typeList = [];
 var notifyTag = "#task";
-alert(m);
 if(m == "view"){
     document.getElementById("dvPanel1").style.display = "block";
     document.getElementById("dvPanel2").style.display = "none";
+}
+if(n != null){
+    document.getElementById("tbNote").value = n;
 }
 
 //events
