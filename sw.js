e4f382eb33811f684c5e72a74f65d4f9959b05c7
vtnw <vtnw@users.noreply.github.com>
2019-08-20 08:30:59 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,34 @@
-self.addEventListener('notificationclick', function(event) {
+var CACHE_NAME = "web";
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+        return cache.addAll("notify.html");
+      })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then(function(response) {
+        if(!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        var responseToCache = response.clone();
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      });
+    });
+  );
+});
+
+self.addEventListener("notificationclick", function(event) {
   //event.notification.close();
   event.waitUntil(clients.openWindow("https://vtnw.github.io/pages/notify.html?data=" + event.notification.title));
 });
