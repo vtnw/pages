8e69e122927242d5b8d162e2b4420b42e1a28931
vtnw <vtnw@users.noreply.github.com>
2019-08-20 08:33:32 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,37 @@
 var CACHE_NAME = "web";
 
 self.addEventListener("install", function(event) {
+  console.log("at install");
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
+      console.log("adding all");
         return cache.addAll("notify.html");
       })
   );
 });
 
 self.addEventListener("fetch", function(event) {
+  console.log("at fetch");
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      if (response) {
+      if (response) {console.log("cache match");
         return response;
       }
       return fetch(event.request).then(function(response) {
-        if(!response || response.status !== 200 || response.type !== 'basic') {
+        if(!response || response.status !== 200 || response.type !== "basic") {console.log("invalid response");
           return response;
         }
         var responseToCache = response.clone();
-        caches.open(CACHE_NAME).then(function(cache) {
+        caches.open(CACHE_NAME).then(function(cache) {console.log("cache put");
           cache.put(event.request, responseToCache);
-        });
+        });console.log("return response");
         return response;
       });
     });
   );
 });
 
-self.addEventListener("notificationclick", function(event) {
+self.addEventListener("notificationclick", function(event) {console.log("notification clicked");
   //event.notification.close();
   event.waitUntil(clients.openWindow("https://vtnw.github.io/pages/notify.html?data=" + event.notification.title));
 });
