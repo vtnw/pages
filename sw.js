3258ef25c40ab38dd534fe3ca16e96cf90db235f
vtnw <vtnw@users.noreply.github.com>
2019-08-20 10:22:03 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,8 +14,8 @@ self.addEventListener("fetch", function(event) {
 			if (response) { return response; }
 			return fetch(event.request).then(function(response) {
 				if(!response || response.status !== 200 || response.type !== "basic") { return response; }
-				var responseToCache = response.clone();
-				caches.open(CACHE_NAME).then(function(cache) { cache.put(event.request, responseToCache); });
+				var responseClone = response.clone();
+				caches.open(CACHE_NAME).then(function(cache) { cache.put(event.request, responseClone); });
 				return response;
 			});
 		})
