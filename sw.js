e51e9f8b3ea769d44dba00447b342d0d55ae2ca0
vtnw <vtnw@users.noreply.github.com>
2020-06-14 12:57:46 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var version = "20200614.1";
+var version = "20200614.2";
 var CACHE_NAME = "pages";
 self.addEventListener("activate", function(event) {
 	event.waitUntil(caches.delete(CACHE_NAME));
